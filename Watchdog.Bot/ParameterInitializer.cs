f1afed93855afd2f53966a28d9ca424cb70070bf
actisPRO <dkarev@protonmail.com>
2023-03-27 00:45:40 +0200

    Parameter initializer

---
+++ Watchdog.Bot/ParameterInitializer.cs
@@ -0,0 +1,26 @@
+﻿using Watchdog.Bot.Constants;
+using Watchdog.Bot.Services.Interfaces;
+
+namespace Watchdog.Bot;
+
+public sealed class ParameterInitializer
+{
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public ParameterInitializer(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public async Task InitializeAsync()
+    {
+        await using var scope = _serviceScopeFactory.CreateAsyncScope();
+        var parameterService = scope.ServiceProvider.GetRequiredService<IParameterService>();
+
+        await parameterService.RegisterParameterAsync<ulong>(new()
+        {
+            Name = ParameterNames.ModerationLogChannelId,
+            Value = 0
+        });
+    }
+}
