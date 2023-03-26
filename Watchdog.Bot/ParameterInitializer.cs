2ef2caf60b141459130bc20d98dd6898b71c55ad
actisPRO <dkarev@protonmail.com>
2023-03-27 00:49:31 +0200

    diagnostics

--- Watchdog.Bot/ParameterInitializer.cs
+++ Watchdog.Bot/ParameterInitializer.cs
@@ -1,4 +1,5 @@
-﻿using Watchdog.Bot.Constants;
+﻿using System.Diagnostics;
+using Watchdog.Bot.Constants;
 using Watchdog.Bot.Services.Interfaces;
 
 namespace Watchdog.Bot;
@@ -6,14 +7,21 @@ namespace Watchdog.Bot;
 public sealed class ParameterInitializer
 {
     private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly ILogger<ParameterInitializer> _logger;
 
-    public ParameterInitializer(IServiceScopeFactory serviceScopeFactory)
+    public ParameterInitializer(IServiceScopeFactory serviceScopeFactory, 
+        ILogger<ParameterInitializer> logger)
     {
         _serviceScopeFactory = serviceScopeFactory;
+        _logger = logger;
     }
 
     public async Task InitializeAsync()
     {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        _logger.LogInformation("Started parameter initialization");
+        
         await using var scope = _serviceScopeFactory.CreateAsyncScope();
         var parameterService = scope.ServiceProvider.GetRequiredService<IParameterService>();
 
@@ -22,5 +30,9 @@ public sealed class ParameterInitializer
             Name = ParameterNames.ModerationLogChannelId,
             Value = 0
         });
+        
+        stopwatch.Stop();
+        _logger.LogInformation("Finished parameter initialization in {ElapsedMilliseconds} ms", 
+            stopwatch.ElapsedMilliseconds);
     }
 }
