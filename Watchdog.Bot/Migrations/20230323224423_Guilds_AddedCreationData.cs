765bffdc4082bd98ab5798db4aae9c7a4409b77d
actisPRO <dkarev@protonmail.com>
2023-03-26 23:25:47 +0200

    Commited forgotten migrations

---
+++ Watchdog.Bot/Migrations/20230323224423_Guilds_AddedCreationData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Watchdog.Bot.Migrations
+{
+    /// <inheritdoc />
+    public partial class Guilds_AddedCreationData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Guilds",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DatabaseEntryCreatedAt",
+                table: "Guilds",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Guilds");
+
+            migrationBuilder.DropColumn(
+                name: "DatabaseEntryCreatedAt",
+                table: "Guilds");
+        }
+    }
+}
