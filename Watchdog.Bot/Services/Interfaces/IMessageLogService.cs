d9efff982f601103cc72ea4ef141b1237ac27fff
Роман Гончаров <ea123.gonch@gmail.com>
2023-03-29 17:16:44 +0300

    Naming fix

--- Watchdog.Bot/Services/Interfaces/IMessageLogService.cs
+++ Watchdog.Bot/Services/Interfaces/IMessageLogService.cs
@@ -1,12 +1,10 @@
-using DSharpPlus;
 using DSharpPlus.Entities;
-using DSharpPlus.EventArgs;
 
 namespace Watchdog.Bot.Services.Interfaces;
 
 public interface IMessageLogService
 {
-    Task ClientOnMessageDeleteReceived(DiscordClient client, DiscordGuild guild, DiscordMessage message, ulong messageLogsChannelId);
-    Task ClientOnMessageUpdateReceived(DiscordClient client, DiscordGuild guild, DiscordMessage messageBefore, DiscordMessage message, ulong messageLogsChannelId);
-    Task ClientOnMessagesBulkDeleteReceived(DiscordClient client, DiscordGuild guild, IReadOnlyList<DiscordMessage> messages, ulong messageLogsChannelId);
+    Task LogDeletedMessage(DiscordGuild guild, DiscordMessage message, ulong messageLogsChannelId);
+    Task LogUpdatedMessage(DiscordGuild guild, DiscordMessage messageBefore, DiscordMessage message, ulong messageLogsChannelId);
+    Task LogBulkDeletedMessages(DiscordGuild guild, IReadOnlyList<DiscordMessage> messages, ulong messageLogsChannelId);
 }
