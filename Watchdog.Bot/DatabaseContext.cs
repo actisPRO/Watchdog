b2a1d0a7f2f5f5ff88af1a2af198f8c85695bcfd
Роман Гончаров <ea123.gonch@gmail.com>
2023-03-29 15:12:04 +0300

    Unused using

--- Watchdog.Bot/DatabaseContext.cs
+++ Watchdog.Bot/DatabaseContext.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.Extensions.Options;
 using Watchdog.Bot.Enums;
-using Watchdog.Bot.Models;
 using Watchdog.Bot.Models.Database;
 using Watchdog.Bot.Options;
 
