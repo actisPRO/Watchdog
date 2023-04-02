05dccc2a0b4e95e1c833a220d2a7b6964d48bbab
VanguardFx <nazarhnatyshyn@hotmail.com>
2023-04-02 22:19:19 +0200

    Added utility for date parsing and formatting

---
+++ Watchdog.Bot.Tests/Utils/TimeUtilsTest.cs
@@ -0,0 +1,166 @@
+﻿using FluentAssertions;
+
+namespace Watchdog.Bot.Utils.Tests;
+
+public class TimeUtilsTests
+{
+
+    
+    [Test]
+    [TestCase("1d10h30m", 1, 10, 30, 0)]
+    [TestCase("1d30m", 1, 0, 30, 0)]
+    [TestCase("30m10s", 0, 0, 30, 10)]
+    [TestCase("7d10h30m", 7, 10, 30, 0)]
+    public void GetFutureDate_ReturnsExpectedDate(string input, int expectedDay, int expectedHour, int expectedMinute, int expectedSecond)
+    {
+        // Arrange
+        var expected = DateTime.UtcNow.Add(new TimeSpan(expectedDay, expectedHour, expectedMinute, expectedSecond));
+
+        // Act
+        var futureDate = TimeParserUtils.GetFutureDate(input);
+
+        // Assert
+        futureDate.Year.Should().Be(expected.Year);
+        futureDate.Month.Should().Be(expected.Month);
+        futureDate.Day.Should().Be(expected.Day);
+        futureDate.Minute.Should().Be(expected.Minute);
+        futureDate.Second.Should().Be(expected.Second);
+    }
+
+    [Test]
+    [TestCase("1d2h30m", 1, 2, 30, 0)]
+    [TestCase("5s", 0, 0, 0, 5)]
+    [TestCase("1d", 1, 0, 0, 0)]
+    [TestCase("30m10s", 0, 0, 30, 10)]
+    [TestCase("7d10h30m", 7, 10, 30, 0)]
+    [TestCase("30m", 0,0,30, 0)]
+    public void ParseTimeSpan_ShouldReturnCorrectTimeSpan(string input, int expectedDay, int expectedHour, int expectedMinute, int expectedSecond)
+    {
+        // Arrange
+        var expected = new TimeSpan(expectedDay, expectedHour, expectedMinute, expectedSecond);
+        
+        // Act
+        var result = TimeParserUtils.ParseTimeSpan(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    public void FormatTimespan_ShouldReturnFormattedString()
+    {
+        // Arrange
+        var time = new TimeSpan(1, 2, 30, 10);
+        const string expected = "1 день 2 часа 30 минут 10 секунд";
+
+        // Act
+        var result = TimeParserUtils.FormatTimespan(time);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().NotBeEmpty();
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    [TestCase(TimeParserUtils.TimeUnit.Days, "1 день")]
+    [TestCase(TimeParserUtils.TimeUnit.Hours, "2 часа")]
+    [TestCase(TimeParserUtils.TimeUnit.Minutes, "30 минут")]
+    [TestCase(TimeParserUtils.TimeUnit.Seconds, "0 секунд")]
+    public void ToCorrectCase_ShouldReturnCorrectCase(TimeParserUtils.TimeUnit timeUnit, string expected)
+    {
+        // Arrange
+        var time = new TimeSpan(1, 2, 30, 0);
+
+        // Act
+        var result = TimeParserUtils.ToCorrectCase(time, timeUnit);
+        
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().NotBeEmpty();
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    [TestCase(TimeParserUtils.TimeUnit.Days, "1 день")]
+    [TestCase(TimeParserUtils.TimeUnit.Hours, "1 час")]
+    [TestCase(TimeParserUtils.TimeUnit.Minutes, "1 минута")]
+    [TestCase(TimeParserUtils.TimeUnit.Seconds, "1 секунда")]
+    public void ToCorrectCase_ShouldHandleSingularNominativeCase(TimeParserUtils.TimeUnit timeUnit, string expected)
+    {
+        // Arrange
+        var time = new TimeSpan(1, 1, 1, 1);
+        
+        // Act
+        var result = TimeParserUtils.ToCorrectCase(time, timeUnit);
+        
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().NotBeEmpty();
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    [TestCase(TimeParserUtils.TimeUnit.Days, "2 дня")]
+    [TestCase(TimeParserUtils.TimeUnit.Hours, "2 часа")]
+    [TestCase(TimeParserUtils.TimeUnit.Minutes, "2 минуты")]
+    [TestCase(TimeParserUtils.TimeUnit.Seconds, "2 секунды")]
+    public void ToCorrectCase_ShouldHandlePluralGenitiveCase(TimeParserUtils.TimeUnit timeUnit, string expected)
+    {
+        // Arrange
+        var time = new TimeSpan(2, 2, 2, 2);
+        
+        // Act
+        var result = TimeParserUtils.ToCorrectCase(time, timeUnit);
+        
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().NotBeEmpty();
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    [TestCase(TimeParserUtils.TimeUnit.Days, "13 дней")]
+    [TestCase(TimeParserUtils.TimeUnit.Hours, "13 часов")]
+    [TestCase(TimeParserUtils.TimeUnit.Minutes, "13 минут")]
+    [TestCase(TimeParserUtils.TimeUnit.Seconds, "13 секунд")]
+    public void ToCorrectCase_ShouldHandlePluralGenitiveCaseWithExceptions(TimeParserUtils.TimeUnit timeUnit, string expected)
+    {
+        // Arrange
+        var time = new TimeSpan(13, 13, 13, 13);
+        
+        // Act
+        var result = TimeParserUtils.ToCorrectCase(time, timeUnit);
+        
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().NotBeEmpty();
+        result.Should().Be(expected);
+    }
+    
+
+    [Test]
+    [TestCase(TimeParserUtils.TimeUnit.Seconds, "0 секунд")]
+    public void ToCorrectCase_ShouldHandlePluralGenitiveCaseForZero(TimeParserUtils.TimeUnit timeUnit, string expected)
+    {
+        // Arrange
+        var time = new TimeSpan(0, 0, 0);
+
+        // Act
+        var result = TimeParserUtils.ToCorrectCase(time, timeUnit);
+        
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().NotBeEmpty();
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    public void ParseTimeSpan_ShouldThrowExceptionForInvalidInput()
+    {
+        // Assert: Expects exception
+
+        Assert.Throws<InvalidOperationException>(() => TimeParserUtils.ParseTimeSpan("invalid"));
+        Assert.Throws<InvalidOperationException>(() => TimeParserUtils.ParseTimeSpan("1d2h30m40sinvalid"));
+    }
+}
---
+++ Watchdog.Bot/Utils/TimeParserUtils.cs
@@ -0,0 +1,109 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Watchdog.Bot.Utils;
+
+public partial class TimeParserUtils
+{
+    public enum TimeUnit
+    {
+        Seconds,
+        Minutes,
+        Hours,
+        Days
+    }
+
+    [GeneratedRegex("^((?<days>\\d+)d)?((?<hours>\\d+)h)?((?<minutes>\\d+)m)?((?<seconds>\\d+)s)?$",
+        RegexOptions.ExplicitCapture | RegexOptions.Compiled | RegexOptions.RightToLeft |
+        RegexOptions.CultureInvariant)]
+    private static partial Regex MyRegex();
+
+    public static DateTime GetFutureDate(string input)
+    {
+        return DateTime.UtcNow.Add(ParseTimeSpan(input));
+    }
+
+    //Парсит формат даты на подобии 1d2h30m, 1d, 30m10s
+    //Добавлены дни, взято отсюда: 
+    //https://stackoverflow.com/questions/47702094/parse-the-string-26h44m3s-to-timespan-in-c-sharp
+    public static TimeSpan ParseTimeSpan(string input)
+    {
+        var m = MyRegex().Match(input.Trim());
+
+        var ds = m.Groups["days"].Success ? int.Parse(m.Groups["days"].Value) : 0;
+        var hs = m.Groups["hours"].Success ? int.Parse(m.Groups["hours"].Value) : 0;
+        var ms = m.Groups["minutes"].Success ? int.Parse(m.Groups["minutes"].Value) : 0;
+        var ss = m.Groups["seconds"].Success ? int.Parse(m.Groups["seconds"].Value) : 0;
+
+        var result = TimeSpan.FromSeconds(ds * 24 * 60 * 60 + hs * 60 * 60 + ms * 60 + ss);
+        if (result.TotalSeconds < 1) throw new InvalidOperationException("Unable to convert time!");
+        return result;
+    }
+
+    public static string FormatTimespan(TimeSpan time)
+    {
+        var ds = time.Days != 0 ? ToCorrectCase(time, TimeUnit.Days) + " " : "";
+        var hs = time.Hours != 0 ? ToCorrectCase(time, TimeUnit.Hours) + " " : "";
+        var ms = time.Minutes != 0 ? ToCorrectCase(time, TimeUnit.Minutes) + " " : "";
+        var ss = time.Seconds != 0 ? ToCorrectCase(time, TimeUnit.Seconds) : "";
+        return time == TimeSpan.Zero ? "0 секунд" : (ds + hs + ms + ss).TrimEnd(' ');
+        //return string.Format("{0:%d}д {0:%h}ч {0:%m}м {0:%s}с", time);
+    }
+
+    public static string ToCorrectCase(TimeSpan time, TimeUnit unit)
+    {
+        /*
+         * Склонение:
+         * Заканчивается на 1, но не на 11 (1, 21, 101, 1121) - номинатив ед. ч.
+         * Заканчивается на 2, 3, 4, но не на 12, 13, 14 (2, 23, 954) - генетив ед. ч.
+         * Заканчивается на 6, 7, 8, 9, 0, 11, 12, 13, 14 - генетив мн. ч.
+         */
+
+        var cts = unit switch
+        {
+            TimeUnit.Days => $"{time:%d}",
+            TimeUnit.Hours => $"{time:%h}",
+            TimeUnit.Minutes => $"{time:%m}",
+            TimeUnit.Seconds => $"{time:%s}",
+            _ => time.ToString()
+        };
+
+        if (cts.EndsWith("1") && !cts.EndsWith("11"))
+            switch (unit)
+            {
+                case TimeUnit.Seconds:
+                    return $"{cts} секунда";
+                case TimeUnit.Minutes:
+                    return $"{cts} минута";
+                case TimeUnit.Hours:
+                    return $"{cts} час";
+                case TimeUnit.Days:
+                    return $"{cts} день";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(unit), unit, null);
+            }
+        if ((cts.EndsWith("2") || cts.EndsWith("3") || cts.EndsWith("4")) &&
+                 !(cts.EndsWith("12") || cts.EndsWith("13") || cts.EndsWith("14")))
+            switch (unit)
+            {
+                case TimeUnit.Seconds:
+                    return $"{cts} секунды";
+                case TimeUnit.Minutes:
+                    return $"{cts} минуты";
+                case TimeUnit.Hours:
+                    return $"{cts} часа";
+                case TimeUnit.Days:
+                    return $"{cts} дня";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(unit), unit, null);
+            }
+
+        return unit switch
+        {
+            TimeUnit.Seconds => $"{cts} секунд",
+            TimeUnit.Minutes => $"{cts} минут",
+            TimeUnit.Hours => $"{cts} часов",
+            TimeUnit.Days => $"{cts} дней",
+            _ => throw new InvalidOperationException("Unable to convert time!")
+        };
+    }
+}
