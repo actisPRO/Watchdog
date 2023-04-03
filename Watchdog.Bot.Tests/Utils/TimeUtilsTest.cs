92b4425e7f08c3e4956993124834d2fba9e231ab
VanguardFx <nazarhnatyshyn@hotmail.com>
2023-04-03 21:19:15 +0200

    Added utility for date parsing and formatting (#31)
    * Added utility for date parsing and formatting
    
    * TimeParserUtils fixes

---
+++ Watchdog.Bot.Tests/Utils/TimeUtilsTest.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using Watchdog.Bot.Utils;
+
+namespace Watchdog.Bot.Tests.Utils;
+
+public class TimeUtilsTests
+{
+    [Test]
+    [TestCase("1d10h30m", 1, 10, 30, 0)]
+    [TestCase("1d30m", 1, 0, 30, 0)]
+    [TestCase("30m10s", 0, 0, 30, 10)]
+    [TestCase("7d10h30m", 7, 10, 30, 0)]
+    public void GetFutureDate_ReturnsExpectedDate(string input, int expectedDay, int expectedHour, int expectedMinute,
+        int expectedSecond)
+    {
+        // Arrange
+        var expected = DateTime.UtcNow.Add(new(expectedDay, expectedHour, expectedMinute, expectedSecond));
+
+        // Act
+        var futureDate = TimeParserUtils.GetFutureDate(input);
+
+        // Assert
+        futureDate.Year.Should().Be(expected.Year);
+        futureDate.Month.Should().Be(expected.Month);
+        futureDate.Day.Should().Be(expected.Day);
+        futureDate.Minute.Should().Be(expected.Minute);
+        futureDate.Second.Should().Be(expected.Second);
+    }
+
+    [Test]
+    [TestCase("1d2h30m", 1, 2, 30, 0)]
+    [TestCase("5s", 0, 0, 0, 5)]
+    [TestCase("1d", 1, 0, 0, 0)]
+    [TestCase("30m10s", 0, 0, 30, 10)]
+    [TestCase("7d10h30m", 7, 10, 30, 0)]
+    [TestCase("30m", 0, 0, 30, 0)]
+    public void ParseTimeSpan_ShouldReturnCorrectTimeSpan(string input, int expectedDay, int expectedHour,
+        int expectedMinute, int expectedSecond)
+    {
+        // Arrange
+        var expected = new TimeSpan(expectedDay, expectedHour, expectedMinute, expectedSecond);
+
+        // Act
+        var result = TimeParserUtils.ParseTimeSpan(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    [TestCase("input")]
+    [TestCase("1d2h30m40sinvalid")]
+    [TestCase("1d2h30m40")]
+    public void ParseTimeSpan_ShouldThrowExceptionForInvalidInput(string input)
+    {
+        // Assert: Expects exception
+
+        var result = () => TimeParserUtils.ParseTimeSpan(input);
+
+        result.Should().Throw<InvalidOperationException>();
+    }
+}
---
+++ Watchdog.Bot/Utils/TimeParserUtils.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Watchdog.Bot.Utils;
+
+public static class TimeParserUtils
+{
+    private static readonly Regex MyRegex = new(
+        "^((?<days>\\d+)d)?((?<hours>\\d+)h)?((?<minutes>\\d+)m)?((?<seconds>\\d+)s)?$",
+        RegexOptions.ExplicitCapture | RegexOptions.Compiled | RegexOptions.RightToLeft |
+        RegexOptions.CultureInvariant);
+
+    public static DateTime GetFutureDate(string input)
+    {
+        return DateTime.UtcNow.Add(ParseTimeSpan(input));
+    }
+
+    //Парсит формат даты на подобии 1d2h30m, 1d, 30m10s
+    //Добавлены дни, взято отсюда: 
+    //https://stackoverflow.com/questions/47702094/parse-the-string-26h44m3s-to-timespan-in-c-sharp
+    public static TimeSpan ParseTimeSpan(string input)
+    {
+        var m = MyRegex.Match(input.Trim());
+
+        var ds = m.Groups["days"].Success ? int.Parse(m.Groups["days"].Value) : 0;
+        var hs = m.Groups["hours"].Success ? int.Parse(m.Groups["hours"].Value) : 0;
+        var ms = m.Groups["minutes"].Success ? int.Parse(m.Groups["minutes"].Value) : 0;
+        var ss = m.Groups["seconds"].Success ? int.Parse(m.Groups["seconds"].Value) : 0;
+
+        var result = TimeSpan.FromSeconds(ds * 24 * 60 * 60 + hs * 60 * 60 + ms * 60 + ss);
+        if (result.TotalSeconds < 1) throw new InvalidOperationException("Unable to convert time!");
+        return result;
+    }
+}
