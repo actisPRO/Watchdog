7009f75005aa95c45c8bb2bf10b330389cd504a8
actisPRO <dkarev@protonmail.com>
2023-04-04 21:45:22 +0200

    Usage statistics model

---
+++ Watchdog.Bot/Migrations/20230404194404_UsageStatistics.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Watchdog.Bot.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsageStatistics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsageStatistics",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "text", nullable: false),
+                    GuildId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Value = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsageStatistics", x => new { x.Key, x.GuildId, x.Date });
+                    table.ForeignKey(
+                        name: "FK_UsageStatistics_Guilds_GuildId",
+                        column: x => x.GuildId,
+                        principalTable: "Guilds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsageStatistics_GuildId",
+                table: "UsageStatistics",
+                column: "GuildId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsageStatistics");
+        }
+    }
+}
---
+++ Watchdog.Bot/Models/Database/UsageStatistic.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Watchdog.Bot.Models.Database;
+
+[PrimaryKey(nameof(Key), nameof(GuildId), nameof(Date))]
+public sealed class UsageStatistic : IEntity
+{
+    public required string Key { get; set; }
+    
+    public required ulong GuildId { get; set; }
+
+    public Guild Guild { get; set; } = default!;
+    
+    public required DateOnly Date { get; set; }
+    
+    public required int Value { get; set; }
+
+    public object[] GetIdentity() => new object[] { Key, GuildId, Date };
+}
